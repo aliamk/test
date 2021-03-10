5e562a1bb2fbdbb3064cfaaed98af793a76c9bfc
unknown <alia.azarian@gmail.com>
2021-03-10 19:14:20 +0000

    Output function producing errors - it mutating the LP_Companies.funds value

--- data.js
+++ data.js
@@ -1,28 +1,3 @@
-const investors_WORLD_timeline = [
-    {
-        "date":"2020-01-22",
-        "list":[
-            {"totalCompanies":150, "PensionFund":120,"SovereignWealthFund":10, "Endowment":20, "id":"BR"},
-            {"totalCompanies":1060, "PensionFund":1000,"SovereignWealthFund":50, "Endowment":10, "id":"CA"},
-            {"totalCompanies":3050, "PensionFund":2500,"SovereignWealthFund":500, "Endowment":50, "id":"US"} 
-        ]
-    },{
-        "date":"2020-06-22",
-        "list":[
-            {"totalCompanies":270, "PensionFund":220,"SovereignWealthFund":20, "Endowment":30, "id":"BR"},
-            {"totalCompanies":1340, "PensionFund":1200, "SovereignWealthFund":80, "Endowment":60, "id":"CA"},
-            {"totalCompanies":3170, "PensionFund":2600, "SovereignWealthFund":500, "Endowment":70, "id":"US"}            
-        ]
-    },{
-        "date":"2021-01-22",
-        "list":[
-            {"totalCompanies":390, "PensionFund":320, "SovereignWealthFund":30, "Endowment":40, "id":"BR"},
-            {"totalCompanies":1905, "PensionFund":1200, "SovereignWealthFund":100, "Endowment":605, "id":"CA"},
-            {"totalCompanies":5695, "PensionFund":5000, "SovereignWealthFund":600, "Endowment":95, "id":"US"}            
-        ]
-    }
-]
-
 const LP_Companies = [
     {
         "name": "Microsoft Pension", 
@@ -66,3 +41,29 @@ const LP_Companies = [
         "type": "Endowment"
     }
 ];
+
+
+const investors_WORLD_timeline = [
+    {
+        "date":"2020-01-22",
+        "list":[
+            {"totalCompanies":150, "PensionFund":120,"SovereignWealthFund":10, "Endowment":20, "id":"BR"},
+            {"totalCompanies":1060, "PensionFund":1000,"SovereignWealthFund":50, "Endowment":10, "id":"CA"},
+            {"totalCompanies":3050, "PensionFund":2500,"SovereignWealthFund":500, "Endowment":50, "id":"US"} 
+        ]
+    },{
+        "date":"2020-06-22",
+        "list":[
+            {"totalCompanies":270, "PensionFund":220,"SovereignWealthFund":20, "Endowment":30, "id":"BR"},
+            {"totalCompanies":1340, "PensionFund":1200, "SovereignWealthFund":80, "Endowment":60, "id":"CA"},
+            {"totalCompanies":3170, "PensionFund":2600, "SovereignWealthFund":500, "Endowment":70, "id":"US"}            
+        ]
+    },{
+        "date":"2021-01-22",
+        "list":[
+            {"totalCompanies":390, "PensionFund":320, "SovereignWealthFund":30, "Endowment":40, "id":"BR"},
+            {"totalCompanies":1905, "PensionFund":1200, "SovereignWealthFund":100, "Endowment":605, "id":"CA"},
+            {"totalCompanies":5695, "PensionFund":5000, "SovereignWealthFund":600, "Endowment":95, "id":"US"}            
+        ]
+    }
+]
--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-
 // Creat a color set
 am4core.useTheme(am4themes_animated);
 am4core.useTheme(am4themes_dataviz);
 var colorSet = new am4core.ColorSet();
 
-// Create main parent container (for everything else to append to)
-const container = document.querySelector('.container')
-container.classList.add('container')
+console.log('array funds: ', LP_Companies[0].funds) // 3520
 
 // TOP ROW: Add the total funds of duplicate fund types (e.g., all pension funds together 21,496)
-let output = LP_Companies.reduce((accumulator, item) => {
-    // Search data with the same 'type' property
-    var type = item.type,
+const output = LP_Companies.reduce((accumulator, item) => {
+    // Search data with matching 'type' properties
+    const type = item.type,
     found = accumulator.find(element => {
         return element.type == type
-    })    
-    console.log('type: ', type)
-    // If the types are the same, add their 'funds' values
+    })        
+    // If the types match, add their 'funds' values together
     if (found) found.funds += item.funds;
     else accumulator.push(item)
     return accumulator
 }, [])
-console.log('output: ', output)
+
+console.log('output: ', output) // 19086
 
 
 // THIRD ROW: Tally instances of duplicate fund types from data LP_Companies e.g., How many Banking companies are there? (2)
@@ -30,62 +27,67 @@ LP_Companies.forEach(obj => {
     let key = JSON.stringify(obj.type)
     counter[key] = (counter[key] || 0) + 1     
 })
-console.log('coutner: ', counter)
+// console.log('coutner: ', counter)
 
 
-// BOTTOM ROW: Add total number of funds all together (take totals from every type of fund and add)
+// BOTTOM ROW: Add total number of funds all together (should be 79,856)
 const totalFunds = output.reduce(function(acc, item) {
     return item.funds + acc
 }, 0)
 
 
-// Create DOM nodes
-function createNodes() { 
 
-    // TOP ROW: Create text containers for fund totals (e.g. the total funds of every pension fund added together)
+// RENDER: CREATE DOM NODES
+const createNodes = () => { 
+
+    // Create main body container (for everything else to append to)
+    const container = document.querySelector('.container')
+    container.classList.add('container')
+
+    // Create parent container div for all rows
     const textContainer = document.createElement('div')
     textContainer.classList.add('text-container')
-    // For each fund type in the database, add up the totals of duplicates
-    Object.entries(output).forEach(value => {
+
+    // TOP ROW: Create buttons/container for fund totals (e.g. the total funds of every pension fund added together)    
+    Object.entries(output).forEach(item => {
         const textFields = document.createElement('div')
         textFields.classList.add('textFieldsContainers') 
         textFields.style.borderColor = colorSet.next()
-        textFields.textContent = `${value[1].type} Totals: ${value[1].funds}`
+        textFields.textContent = `${item[1].type} Totals: ${item[1].funds}`
         textContainer.append(textFields)
         container.append(textContainer)
     })
 
-
-    // SECOND ROW: Create containers/buttons for COMPANY NAME and their 'funds' properties 
+    // SECOND ROW: Create buttons/container for COMPANY NAME and their 'funds' properties 
     const companyContainers = document.createElement('div')
     companyContainers.classList.add('companyContainers')  
     // Company names
-    LP_Companies.forEach((item) => {
+    LP_Companies.forEach(item => {        
         const buttonCompany = document.createElement('button')
         buttonCompany.classList.add('buttonCompany')        
         buttonCompany.style.borderColor = colorSet.next()
         buttonCompany.textContent = `${item.name}: ${item.funds}`
         companyContainers.append(buttonCompany)        
-        container.append(companyContainers)
-        console.log(item)
+        container.append(companyContainers)      
+        console.log('companyContainers', item)  
     })  
 
 
-    // Create container for FUND TYPE buttons (Banking: 2 )
+    // THIRD ROW: Create buttons/container for FUND TYPE buttons (Banking: 2 )
     const typeContainers = document.createElement('div')
     typeContainers.classList.add('typeContainers')
-    container.append(typeContainers)
-    // Create a button for each database entry and insert fund type and tallied instances
-    Object.entries(counter).forEach(value => {
+    container.append(typeContainers)    
+    // Create a button for each fund type and their tallied instances
+    Object.entries(counter).forEach(item => {
         const buttonType = document.createElement('button')
         buttonType.classList.add('buttonType')         
         buttonType.style.borderColor = colorSet.next();
-        buttonType.textContent = `${JSON.parse(value[0])}: ${value[1]}`
-        buttonType.setAttribute('onClick', `filterFundTypes('${value[0]}')`)
+        buttonType.textContent = `${JSON.parse(item[0])}: ${item[1]}`
+        buttonType.setAttribute('onClick', `filterFundTypes('${item[0]}')`)
         typeContainers.append(buttonType)    
     })
 
-    // Every fund added together (79,856)
+    // FINAL ROW: fund added together (79,856)
     const buttonTotalFunds = document.createElement('button')
     buttonTotalFunds.classList.add('totalButton')  
     buttonTotalFunds.textContent = `Every fund's value added together: ${totalFunds}`
@@ -93,9 +95,12 @@ function createNodes() {
 }
 
 // Click buttons
-const filterFundTypes = (value) => {
-    console.log('value: ', value)
+const filterFundTypes = (item) => {
+
+    console.log('Fund Name: ', item)    
 }
 
 // Onload
 createNodes()
+
+//.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
