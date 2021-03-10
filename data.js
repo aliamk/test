88f1fc48987afec1b70bf7a00738228ad4f55a06
unknown <alia.azarian@gmail.com>
2021-03-10 13:47:46 +0000

    initial commit

---
+++ data.js
@@ -0,0 +1,68 @@
+const investors_WORLD_timeline = [
+    {
+        "date":"2020-01-22",
+        "list":[
+            {"totalCompanies":150, "PensionFund":120,"SovereignWealthFund":10, "Endowment":20, "id":"BR"},
+            {"totalCompanies":1060, "PensionFund":1000,"SovereignWealthFund":50, "Endowment":10, "id":"CA"},
+            {"totalCompanies":3050, "PensionFund":2500,"SovereignWealthFund":500, "Endowment":50, "id":"US"} 
+        ]
+    },{
+        "date":"2020-06-22",
+        "list":[
+            {"totalCompanies":270, "PensionFund":220,"SovereignWealthFund":20, "Endowment":30, "id":"BR"},
+            {"totalCompanies":1340, "PensionFund":1200, "SovereignWealthFund":80, "Endowment":60, "id":"CA"},
+            {"totalCompanies":3170, "PensionFund":2600, "SovereignWealthFund":500, "Endowment":70, "id":"US"}            
+        ]
+    },{
+        "date":"2021-01-22",
+        "list":[
+            {"totalCompanies":390, "PensionFund":320, "SovereignWealthFund":30, "Endowment":40, "id":"BR"},
+            {"totalCompanies":1905, "PensionFund":1200, "SovereignWealthFund":100, "Endowment":605, "id":"CA"},
+            {"totalCompanies":5695, "PensionFund":5000, "SovereignWealthFund":600, "Endowment":95, "id":"US"}            
+        ]
+    }
+]
+
+const LP_Companies = [
+    {
+        "name": "Microsoft Pension", 
+        "totalFunds": 3520,
+        "type": "Pension Fund"
+    },{
+        "name": "Apple Pension",
+        "totalFunds": 7410,
+        "type": "Pension Fund"
+    },{
+        "name": "Japan Development Bank",
+        "totalFunds": 2410,
+        "type": "Development Bank"
+    },{
+        "name": "Teacher's Pension",
+        "totalFunds": 8156,
+        "type": "Pension Fund"
+    },{
+        "name": "Norway Development Bank",
+        "totalFunds": 3410,
+        "type": "Development Bank"
+    },{
+        "name": "England Wealth Fund",
+        "totalFunds": 1020,
+        "type": "Sovereign Wealth Fund"
+    },{
+        "name": "Oxford Endowment Fund",
+        "totalFunds": 1300,
+        "type": "Endowment"
+    },{
+        "name": "Deutsche Bank",
+        "totalFunds": 13210,
+        "type": "Banking"
+    },{
+        "name": "HSBC",
+        "totalFunds": 16210,
+        "type": "Banking"
+    },{
+        "name": "Cambridge Endowment Fund",
+        "totalFunds": 23210,
+        "type": "Endowment"
+    }
+];
---
+++ script.js
@@ -0,0 +1,99 @@
+
+// Creat a color set
+am4core.useTheme(am4themes_animated);
+am4core.useTheme(am4themes_dataviz);
+
+var colorSet = new am4core.ColorSet();
+
+const container = document.querySelector('.container')
+container.classList.add('container')
+
+
+// Tally instances of duplicate fund types from data LP_Companies e.g., How many Banking companies are there? (2)
+let counter = {}
+LP_Companies.forEach(obj => {
+    let key = JSON.stringify(obj.type)
+    counter[key] = (counter[key] || 0) + 1     
+})
+console.log('counterObject: ', counter)
+
+
+// Add the total funds of duplicate fund types (e.g., all pension funds together)
+let output = LP_Companies.reduce((accumulator, item) => {
+    var type = item.type,
+    found = accumulator.find(element => {
+        return element.type == type
+    })
+    if (found) found.totalFunds += item.totalFunds;
+    else accumulator.push(item)
+    return accumulator
+}, [])
+console.log('output: ', output)
+
+// Add total number of funds all together
+const totalFunds = output.reduce(function(acc, item) {
+    return item.totalFunds + acc
+}, 0)
+
+console.log('totalFunds: ', totalFunds)
+
+
+function createNodes() { 
+    // Create containers/buttons for COMPANY NAME and their totalFunds properties 
+    const companyContainers = document.createElement('div')
+    companyContainers.classList.add('companyContainers')  
+    // Company names
+    LP_Companies.forEach((item) => {
+        const buttonCompany = document.createElement('button')
+        buttonCompany.classList.add('buttonCompany')        
+        buttonCompany.style.borderColor = colorSet.next()
+        buttonCompany.textContent = `${item.name}: ${item.totalFunds}`
+        companyContainers.append(buttonCompany)        
+        container.append(companyContainers)
+        LP_Companies.forEach((item) => { console.log(item)})
+    })
+    
+
+    // Create text containers for fund totals 
+    const textContainer = document.createElement('div')
+    textContainer.classList.add('text-container')
+    // For each fund type in the database, add up the totals of duplicates
+    Object.entries(output).forEach(value => {
+        const textFields = document.createElement('div')
+        textFields.classList.add('textFieldsContainers') 
+        textFields.style.borderColor = colorSet.next()
+        textFields.textContent = `${value[1].type} Totals: ${value[1].totalFunds}`
+        textContainer.append(textFields)
+        container.append(textContainer)
+    })
+
+
+
+    // Create container for FUND TYPE buttons (Banking: 2 )
+    const typeContainers = document.createElement('div')
+    typeContainers.classList.add('typeContainers')
+    container.append(typeContainers)
+    // Create a button for each database entry and insert fund type and tallied instances
+    Object.entries(counter).forEach(value => {
+        const buttonType = document.createElement('button')
+        buttonType.classList.add('buttonType')         
+        buttonType.style.borderColor = colorSet.next();
+        buttonType.textContent = `${JSON.parse(value[0])}: ${value[1]}`
+        buttonType.setAttribute('onClick', `filterFundTypes('${value[0]}')`)
+        typeContainers.append(buttonType)    
+    })
+
+    // Every fund added together
+    const buttonTotalFunds = document.createElement('button')
+    buttonTotalFunds.classList.add('totalButton')  
+    buttonTotalFunds.textContent = `Every fund's value added together: ${totalFunds}`
+    container.append(buttonTotalFunds)
+}
+
+// Click buttons
+const filterFundTypes = (value) => {
+    console.log('value: ', value)
+}
+
+// Onload
+createNodes()
